be2a7167d1bbbc1db91eaf00920fa0f3ffd4b033
mekline <melissa.e.kline@gmail.com>
2015-03-22 17:23:35 -0400

    Coded thesis data, commented/expanded analyses for E3

--- Kid Choice Experiments/SubDrop.R
+++ Kid Choice Experiments/SubDrop.R
@@ -1,5 +1,7 @@
 #Analysis of the SubjectDrop study!
 
+#setwd(mydir)
+
 #Reading in all libraries that we'll use
 library(irr)
 library(stringr)
@@ -20,7 +22,7 @@ directory = getwd()
 subtable = data.frame(NULL)
 
 #Load csv with Alldata into variable
-subtable = read.csv(paste0(directory, "/SubDropSpeakers_Data_31215.csv"), header = TRUE, stringsAsFactors = FALSE)
+subtable = read.csv(paste0(directory, "/SubDropSpeakers_Data_32115_thesis.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 
 #Fix some NA columns
@@ -36,33 +38,25 @@ subtable[is.na(subtable)] <- 0
 
 #Drop non-included kids!
 subtable <- subtable[subtable$Include.subject. == "1",]
-
-#Drop early version of kidsecret (confusing instructions)
-subtable <- subtable[subtable$Experiment != "KidSecret",]
-
-##Look at n kids in sub-experiments (good for checking updates on n subjects needed per condition)
-##How many kids of each Age, Experiment, Condition?
-#with(subtable, tapply(as.numeric(as.character(Include.subject.)), list(Experiment, Condition, Age.Years), sum.na.rm), drop=TRUE)
-#
-##Get info for individual sub-experiments (good for updating 'subjects needed' on ongoing exps)
-#subtable$Age.Years <- as.numeric(as.character(subtable$Age.Years))
-#subtable$Gender <- subtable$Gender..Guessed.from.Name.Appearance.
-#PSecret <- subtable[subtable$Experiment == "ParentSecret",]
-#KSecret <- subtable[subtable$Experiment == "KidSecret-New",]
-#
-#Parent56 <- PSecret[PSecret$Age.Years >4,]
-#Parent34 <- PSecret[PSecret$Age.Years < 5,]
-#Kid56 <- KSecret[KSecret$Age.Years>4,]
-#with(Parent34, tapply(as.numeric(as.character(Include.subject.)), list(Condition, Age.Years, Gender), sum.na.rm), drop=TRUE)
-#with(Kid56, tapply(as.numeric(as.character(Include.subject.)), list(Condition, Age.Years, Gender), sum.na.rm), drop=TRUE)
-
+#New - choose stricter inclusion criteria.., following new paradigm rules
+subtable <- subtable[subtable$Strict.include == 1,]
 
 #Choose ParentSecret on older children
 subtable <- subtable[subtable$Experiment == "ParentSecret",]
-#subtable <- subtable[subtable$Age.Years > 4,]
+subtable <- subtable[subtable$Age.Years > 4,]
 
-#Optional - choose stricter inclusion criteria..
-subtable <- subtable[subtable$Strict.include == 1,]
+
+
+#Look at n kids in sub-experiments (this is good for checking updates on n subjects needed per condition)
+#How many kids of each Age, Experiment, Condition?
+
+subtable$Age.Years <- as.numeric(as.character(subtable$Age.Years))
+subtable$Gender <- subtable$Gender..Guessed.from.Name.Appearance.
+
+with(subtable, tapply(as.numeric(as.character(Strict.include)), list(Condition, Age.Years), sum.na.rm), drop=TRUE)
+
+
+
 
 
 #############################################
@@ -71,21 +65,20 @@ subtable <- subtable[subtable$Strict.include == 1,]
 #SD: 'subject drop' is the 'correct answer', other name for this condition is 'two fruits'
 #OD: aka 'two animals'
 
+#Note- we tried 2-trial and 4-trial versions of the task.  With within-subj 4-trial version, we saw big carryover effects. So, analyze just 1st 2 trials
+#from all versions togther.
+
+subtable$oldCond <- subtable$Condition
+subtable[subtable$Condition == "SDOD",]$Condition <- "SD"
+subtable[subtable$Condition == "SDSD",]$Condition <- "SD"
+subtable[subtable$Condition == "ODSD",]$Condition <- "OD"
+subtable[subtable$Condition == "ODOD",]$Condition <- "OD"
+
 subtable$isPragChoiceA <- "NA"
 subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 0
 subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 1
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 1
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 0
-
-subtable[subtable$Condition == "SDOD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 0
-subtable[subtable$Condition == "SDOD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 1
-subtable[subtable$Condition == "ODSD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 1
-subtable[subtable$Condition == "ODSD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 0
-
-subtable[subtable$Condition == "SDSD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 0
-subtable[subtable$Condition == "SDSD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 1
-subtable[subtable$Condition == "ODOD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 1
-subtable[subtable$Condition == "ODOD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 0
 
 subtable$isPragChoiceB <- "NA"
 subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 0
@@ -93,37 +86,23 @@ subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == 
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 1
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 0
 
-subtable[subtable$Condition == "SDOD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 0
-subtable[subtable$Condition == "SDOD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 1
-subtable[subtable$Condition == "ODSD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 1
-subtable[subtable$Condition == "ODSD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 0
-
-subtable[subtable$Condition == "SDSD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 0
-subtable[subtable$Condition == "SDSD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 1
-subtable[subtable$Condition == "ODOD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 1
-subtable[subtable$Condition == "ODOD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 0
-
 subtable$isPragChoiceA <- as.numeric(as.character(subtable$isPragChoiceA))
 subtable$isPragChoiceB <- as.numeric(as.character(subtable$isPragChoiceB))
 
+#Express this as # chose 'correct'
 subtable$pragChoiceScore <- subtable$isPragChoiceA + subtable$isPragChoiceB
 
+#...Or as # chose to drop the object
 subtable$choseObjectDrop <- subtable$pragChoiceScore
 subtable[subtable$Condition == "SD",]$choseObjectDrop <- 2-subtable[subtable$Condition == "SD",]$pragChoiceScore
-subtable[subtable$Condition == "SDOD",]$choseObjectDrop <- 2-subtable[subtable$Condition == "SDOD",]$pragChoiceScore
-subtable[subtable$Condition == "SDSD",]$choseObjectDrop <- 2-subtable[subtable$Condition == "SDSD",]$pragChoiceScore
 
 
 ####################################
-#Descriptive stats for graph
-
-subtable[subtable$Condition == "SDOD",]$Condition <- "SD"
-subtable[subtable$Condition == "SDSD",]$Condition <- "SD"
-subtable[subtable$Condition == "ODSD",]$Condition <- "OD"
-subtable[subtable$Condition == "ODOD",]$Condition <- "OD"
+#Descriptive stats for graph (Developmental, small sample, so we'll present hist. of kids choosing each asnwer, rather than proportion scores)
 
 table(subtable$Condition, subtable$choseObjectDrop)
 table(subtable$Condition, subtable$pragChoiceScore)
+
 
 ####################################
 # Analysis
@@ -140,19 +119,12 @@ sub.long = wideToLong(subtable,within="trial", sep='_')
 sub.long$choseObjectDrop <- sub.long$pragChoice
 sub.long[sub.long$Condition == "SD",]$choseObjectDrop <- 1-sub.long[sub.long$Condition == "SD",]$pragChoice
 
-# Logistic Regression models.  No (Condition|Subject) random effect because condition was varied between subjects
-full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial), data=sub.long, family="binomial")
-summary(full_maximal_model)
+# Logistic Regression model.  No (Condition|Subject) random effect because condition was varied between subjects
+#full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=sub.long, family="binomial")
+#Doesn't converge! Remove random effects
+full_maximal_model <- lmer(choseObjectDrop ~ Condition + (1|trial) + (1|Subject), data=sub.long, family="binomial")
 
-#Alternate way to evaluate the model: compare to model w/o fixed effect
-no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial), data=sub.long, family="binomial")
+#compare to model w/o fixed effect
+no_fixed <- lmer(choseObjectDrop ~ 1 + (1|trial) + (1|Subject), data=sub.long, family="binomial")
 anova(full_maximal_model, no_fixed)
-
-###############################
-# Exploratory stats with the tiny group of correctly done kids!
-sub.strict <- sub.long[sub.long$Strict.include == 1,]
-full_maximal_strict <- lmer(choseObjectDrop ~ Condition + (Condition|trial), data=sub.strict, family="binomial")
-summary(full_maximal_strict)
-no_fixed_strict <- lmer(choseObjectDrop ~ 1 + (Condition|trial), data=sub.strict, family="binomial")
-anova(full_maximal_strict, no_fixed_strict)
 
