280c75b9710c7f6da986007941787103f6cf1980
mekline <melissa.e.kline@gmail.com>
2015-08-18 16:40:38 -0400

    Now the analyses as of summer 2015 are all pretty

--- Kid Choice Experiments/SubDrop.R
+++ Kid Choice Experiments/SubDrop.R
@@ -1,10 +1,8 @@
 #Analysis of the SubjectDrop study!
 
-#newstuff
-
 #setwd(mydir)
 
-#Reading in all libraries that we'll use
+#Reading in all libraries that we (might) use
 library(irr)
 library(stringr)
 library(languageR)
@@ -27,13 +25,12 @@ subtable = data.frame(NULL)
 subtable = read.csv(paste0(directory, "/SubDropSpeakers_Data.csv"), header = TRUE, stringsAsFactors = FALSE)
 
 
-#Fix some NA columns
-subtable[is.na(subtable$Strict.include),]$Strict.include <- 0
+#Fix some badly formatted columns
 subtable$Kid.Response.A...Prag.Choice. <- as.character (subtable$Kid.Response.A...Prag.Choice.)
 subtable$Kid.Response.B...Prag.Choice. <- as.character (subtable$Kid.Response.B...Prag.Choice.)
+subtable$Gender <- subtable$Gender..Guessed.from.Name.Appearance.
 
 subtable[is.na(subtable)] <- 0
-#subtable[is.na(subtable$Include.subject),]$Include.subject <- 0
 
 #Fix age calculations!
 subtable$Age.Years <- as.numeric(as.character(subtable$Age.Years))
@@ -43,30 +40,18 @@ subtable$Days.Old <- as.numeric(as.character(subtable$Days.Old))
 #Pick subset of data to analyze
 
 #Drop non-included kids!
-#subtable <- subtable[subtable$Include.subject. == "1",]
-#New - choose stricter inclusion criteria.., following new paradigm rules
-subtable <- subtable[subtable$Strict.include == 1,]
 
-#Choose ParentSecret study version
-subtable <- subtable[subtable$Experiment == "ParentSecret",]
-#subtable <- subtable[subtable$Age.Years > 4,]
+#Right here would be the place to print a table of exclusion reasons.
 
+#New - chose stricter inclusion criteria.., following new paradigm rules (= inconsistent implementation in 1st run)
+subtable <- subtable[subtable$Final.Include == 1,]
 
-
-#Look at n kids in sub-experiments (this is good for checking updates on n subjects needed per condition)
-#How many kids of each Age, Experiment, Condition?
-
-subtable$Age.Years <- as.numeric(as.character(subtable$Age.Years))
-subtable$Gender <- subtable$Gender..Guessed.from.Name.Appearance.
-
-with(subtable, tapply(as.numeric(as.character(Strict.include)), list(Condition, Age.Years), sum.na.rm), drop=TRUE)
-
-
-
+#Choose ParentSecret and ParentSecretControl study versions
+subtable <- subtable[subtable$Experiment == "ParentSecret" | subtable$Experiment == "ParentSecretControl" | subtable$Experiment == "ParentSecretControl2" ,]
 
 
 #############################################
-# Recode variables
+# Recode condition variables
 
 #SD: 'subject drop' is the 'correct answer', other name for this condition is 'two fruits'
 #OD: aka 'two animals'
@@ -80,27 +65,34 @@ subtable[subtable$Condition == "SDSD",]$Condition <- "SD"
 subtable[subtable$Condition == "ODSD",]$Condition <- "OD"
 subtable[subtable$Condition == "ODOD",]$Condition <- "OD"
 
+## Code Correctness!  For main experiment, correctness = chose the pragmatic one; For cont, correctness = chose the correct one! (this is the same! wrong answers differ though)
+
 subtable$isPragChoiceA <- "NA"
-subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 0
 subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 1
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 1
+subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "monkey eat",]$isPragChoiceA <- 0
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.A...Prag.Choice. == "eat orange",]$isPragChoiceA <- 0
+subtable[subtable$Condition == "SD" & subtable$Kid.Response.A...Prag.Choice. == "eat banana",]$isPragChoiceA <- 0
+subtable[subtable$Condition == "OD" & subtable$Kid.Response.A...Prag.Choice. == "duck eat",]$isPragChoiceA <- 0
 
 subtable$isPragChoiceB <- "NA"
-subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 0
 subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 1
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 1
+subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "girl pet",]$isPragChoiceB <- 0
 subtable[subtable$Condition == "OD" & subtable$Kid.Response.B...Prag.Choice. == "pet dog",]$isPragChoiceB <- 0
+subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "pet cat",]$isPragChoiceB <- 0
+subtable[subtable$Condition == "SD" & subtable$Kid.Response.B...Prag.Choice. == "pet kitty",]$isPragChoiceB <- 0 #lexical alternative!
+subtable[subtable$Condition == "OD" & subtable$Kid.Response.B...Prag.Choice. == "boy pet",]$isPragChoiceB <- 0
 
-
-#One kid didn't answer on trial 1 and will need to be dropped
+#A few kis didn't answer on one trial and will need to be manually dropped
 subtable <- subtable[subtable$isPragChoiceA != "NA",]
+subtable <- subtable[subtable$isPragChoiceB != "NA",]
 subtable$isPragChoiceA <- as.numeric(as.character(subtable$isPragChoiceA))
 subtable$isPragChoiceB <- as.numeric(as.character(subtable$isPragChoiceB))
 
 
 
-#Express this as # chose 'correct'
+#Express this as # chose 'correct' across experiment
 subtable$pragChoiceScore <- subtable$isPragChoiceA + subtable$isPragChoiceB
 
 #...Or as # chose to drop the object
@@ -109,99 +101,110 @@ subtable[subtable$Condition == "SD",]$choseObjectDrop <- 2-subtable[subtable$Con
 
 
 ####################################
-#Descriptive stats for graph (Developmental, small sample, so we'll present hist. of kids choosing each asnwer, rather than proportion scores)
+#Descriptive stats for graphing (Developmental, small sample, so we'll present hist. of kids choosing each asnwer, rather than proportion scores)
 
-table(subtable$Condition, subtable$choseObjectDrop)
-table(subtable$Condition, subtable$pragChoiceScore)
+#Time to split up the kids into Main and Control experiments 
 
+maintable <- subtable[subtable$Experiment == "ParentSecret",] 
+conttable <- subtable[subtable$Experiment == "ParentSecretControl" | subtable$Experiment == "ParentSecretControl2",] 
+
+#Toss older accidental participant from conttable, it's just for 3-4yos
+conttable <- conttable[conttable$Age.Years < 5,]
+
+
+#Look at n kids in sub-experiments (this is good for checking updates on n subjects needed per condition)
+#How many kids of each Age, Experiment, Condition?
+
+with(maintable, tapply(as.numeric(as.character(Final.Include)), list(Condition, Age.Years), sum.na.rm), drop=TRUE)
+with(conttable, tapply(as.numeric(as.character(Final.Include)), list(Condition, Age.Years), sum.na.rm), drop=TRUE)
+
+
+
+#Everyone! Not that useful while there's different ns per group...
+table(maintable$Condition, maintable$pragChoiceScore, maintable$Age.Years)
+table(conttable$Condition, conttable$pragChoiceScore, conttable$Age.Years)
+
+
+#Make sure factors are coded correctly, and melt the dataset for logistic analyses
+
+maintable$Condition <- as.factor(maintable$Condition)
+maintable$Subject <- as.factor(maintable$Subject..)
+maintable$choseObjectDrop <- as.factor(maintable$choseObjectDrop)
+maintable$pragChoice_1 <- maintable$isPragChoiceA
+maintable$pragChoice_2 <- maintable$isPragChoiceB
+
+#Get the objective coding scheme back :)
+main.long = wideToLong(maintable,within="trial", sep='_')
+main.long$choseObjectDrop <- main.long$pragChoice
+main.long[main.long$Condition == "SD",]$choseObjectDrop <- 1-main.long[main.long$Condition == "SD",]$pragChoice
+
+conttable$Condition <- as.factor(conttable$Condition)
+conttable$Subject <- as.factor(conttable$Subject..)
+conttable$choseObjectDrop <- as.factor(conttable$choseObjectDrop)
+conttable$pragChoice_1 <- conttable$isPragChoiceA
+conttable$pragChoice_2 <- conttable$isPragChoiceB
+
+#Get the objective coding scheme back :)
+cont.long = wideToLong(conttable,within="trial", sep='_')
+cont.long$choseObjectDrop <- cont.long$pragChoice
+cont.long[cont.long$Condition == "SD",]$choseObjectDrop <- 1-cont.long[cont.long$Condition == "SD",]$pragChoice
 
 ####################################
-# Analysis
+# Analysis!
 
-#First make sure factors are coded correctly, and melt the dataset
-
-subtable$Condition <- as.factor(subtable$Condition)
-subtable$Subject <- as.factor(subtable$Subject..)
-subtable$choseObjectDrop <- as.factor(subtable$choseObjectDrop)
-subtable$pragChoice_1 <- subtable$isPragChoiceA
-subtable$pragChoice_2 <- subtable$isPragChoiceB
-
-sub.long = wideToLong(subtable,within="trial", sep='_')
-sub.long$choseObjectDrop <- sub.long$pragChoice
-sub.long[sub.long$Condition == "SD",]$choseObjectDrop <- 1-sub.long[sub.long$Condition == "SD",]$pragChoice
-
+###
+#Question #1 - is choice of OD puppet sensitive to condition?
 # Logistic Regression model.  No (Condition|Subject) random effect because condition was varied between subjects
-full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=sub.long, family="binomial")
+full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=main.long, family="binomial")
 
 #compare to model w/o fixed effect
-no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=sub.long, family="binomial")
+no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=main.long, family="binomial")
 anova(full_maximal_model, no_fixed)
 
+#Answer #1 marginal (with all ages included)
+
+###
+#Question 2: Does age influence choice patterns?
+
+#Scale age (z score), else we get convergence problems
+main.long$Scaled.Days.Old <- scale(main.long$Days.Old)
+
+# Logistic Regression model.  No (Condition|Subject) random effect because condition was varied between subjects
+full_maximal_model <- lmer(choseObjectDrop ~ Condition*Scaled.Days.Old + (Condition|trial) + (1|Subject), data=main.long, family="binomial")
+no_age <- lmer(choseObjectDrop ~ Condition+Scaled.Days.Old + (Condition|trial) + (1|Subject), data=main.long, family="binomial")
+anova(full_maximal_model, no_age)
+
+#Answer #2 yes there is an effect of age!
+
+#Question 3 What if we consider only older kids? 4yos and up? (Justification - see conttable analyses)
+
+older.long <- main.long[main.long$Age.Years > 4,] 
+# Logistic Regression model.  No (Condition|Subject) random effect because condition was varied between subjects
+full_maximal_model_older <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=older.long, family="binomial")
+
+#compare to model w/o fixed effect
+no_fixed_older <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=older.long, family="binomial")
+anova(full_maximal_model_older, no_fixed_older)
+
+#Answer #3 yup
+
+
+#Question #4 Can the littles do it at all? Here we just want to ask if the choices are on average different from 50% (Remember that 'conditions' here are just two kinds of true/false!)
+#(MK goes away and decides the most reasonable way to do this...)
 
 
 
+######Graphs
 
-######AGE EFFECTS
-
-#graphs split by age
-subtable3 <- subtable[subtable$Age.Years < 4,]
-subtable4 <- subtable[subtable$Age.Years > 3 & subtable$Age.Years < 5,]
-subtable5 <- subtable[subtable$Age.Years > 4 & subtable$Age.Years < 6,]
-subtable6 <- subtable[subtable$Age.Years > 5,]
-
-table(subtable3$Condition, subtable3$choseObjectDrop)
-table(subtable4$Condition, subtable4$choseObjectDrop)
-table(subtable5$Condition, subtable5$choseObjectDrop)
-table(subtable6$Condition, subtable6$choseObjectDrop)
 
 #Make a pretty dot graph I hope?
-Scores <- aggregate(sub.long$pragChoice, list(sub.long$Subject), sum)
+Scores <- aggregate(main.long$pragChoice, list(main.long$Subject), sum)
 names(Scores) <- c("Subject","Score")
-Ages <- sub.long[ !duplicated(sub.long$Subject), c("Subject","Days.Old")]
+Ages <- main.long[ !duplicated(main.long$Subject), c("Subject","Days.Old")]
 
 foo <- merge(Scores,Ages)
 
 foo$JitScore <- jitter(foo$Score)
 
 plot( foo$Days.Old, foo$JitScore)
-
-
-#ANALYSIS by age
-
-
-sub.long56 <- sub.long[sub.long$Age.Years>4,]
-sub.long34 <- sub.long[sub.long$Age.Years<5,]
-sub.long4 <- sub.long34[sub.long34$Age.Years>3,]
-
-#Within age groups
-#5 and 6
-full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=sub.long56, family="binomial")
-#compare to model w/o fixed effect
-no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=sub.long56, family="binomial")
-anova(full_maximal_model, no_fixed)
-
-#3 and 4
-full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=sub.long34, family="binomial")
-#compare to model w/o fixed effect
-no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=sub.long34, family="binomial")
-anova(full_maximal_model, no_fixed)
-
-#4s alone
-full_maximal_model <- lmer(choseObjectDrop ~ Condition + (Condition|trial) + (1|Subject), data=sub.long4, family="binomial")
-#compare to model w/o fixed effect
-no_fixed <- lmer(choseObjectDrop ~ 1 + (Condition|trial) + (1|Subject), data=sub.long4, family="binomial")
-anova(full_maximal_model, no_fixed)
-
-######
-#Now try age as a factor??
-
-#Scale age (z score), else we get convergence problems
-sub.long$Scaled.Days.Old <- scale(sub.long$Days.Old)
-
-# Logistic Regression model.  No (Condition|Subject) random effect because condition was varied between subjects
-full_maximal_model <- lmer(choseObjectDrop ~ Condition*Scaled.Days.Old + (Condition|trial) + (1|Subject), data=sub.long, family="binomial")
-no_age <- lmer(choseObjectDrop ~ Condition+Scaled.Days.Old + (Condition|trial) + (1|Subject), data=sub.long, family="binomial")
-anova(full_maximal_model, no_age)
-
-
 
